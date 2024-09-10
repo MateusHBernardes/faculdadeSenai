f6a41ddb6532fc6e2a05ad320266c3db00d16d3d
MateusHBernardes <mateushenriquebernardes@gmail.com>
2024-09-09 21:55:44 -0300

    falta terminar o codigo

--- AtividadesDeExemplo/IfElseIfeElse.java
+++ AtividadesDeExemplo/IfElseIfeElse.java
@@ -2,7 +2,18 @@ import javax.swing.JOptionPane;
 
 public class ScanPrint {
 
-    int numero;
+    public static void main(String[] args) {
+        int numero;
 
-    numero = Integer.
+        numero = Interg.parseInt(JOptionPane.showInputDialog("Informe um número: "));
+
+        if (numero < 0) {
+            JOptionPane.showMessageDialog(null, "O número informado é menor que Zero", "Se, Senão Se e Senão > If, Else If e Else",
+            JOptionPane.INFORMATION_MESSAGE);
+        } else if (numero == 0) {
+            JOptionPane.showMessageDialog(null, args, null, numero);
+        }
+    }
+
+    
 }
