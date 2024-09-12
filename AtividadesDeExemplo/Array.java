6c2f22ac92b12bbd321c36394dc1da144c08bfd1
MateusHBernardes <mateushenriquebernardes@gmail.com>
2024-09-11 21:46:45 -0300

    error array

---
+++ AtividadesDeExemplo/Array.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+
+public class Array {
+    public static void main(String[] args) {
+        int valores [] = new int [2];
+        Double media = 0.0;
+
+        JOptionPane.showMessageDialog(null, "Informe 2 e saiba a média deles",
+            "Vetor > Array", JOptionPane.INFORMATION_MESSAGE);
+
+        for (int i = 0; i < 2; i++) {
+            valores[i] = Integer.parseInt(JOptionPane.showInputDialog("Informe o " + (i + 1) + "º valor:"));
+
+            media += valores[i];
+        }
+
+        media += valores[i];
+    }
+
+    media = meda / valores.length; //Esta função retorna o tamanho do array
+        
+    JOptionPane.showMessageDialog(null, "A média é:" + media,
+        "Vetor > Array", JOptionPane.INFORMATION_MESSAGE);
+}
