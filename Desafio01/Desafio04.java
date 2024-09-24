e63c7304682368c7325c9e7c0faceb451757c589
mateushbernardes <mateushenriquebernardes@gmail.com>
2024-09-23 21:12:48 -0300

    desafio 04 finished

--- Desafio01/Desafio04.java
+++ Desafio01/Desafio04.java
@@ -1,3 +1,16 @@
+
+import javax.swing.JOptionPane;
+
+
 public class Desafio04 {
+    public static void main(String[] args) {
+        
+        String texto = JOptionPane.showInputDialog("Informe uma palavra: \n");
+        
+        int resultado = texto.length();
+
+        JOptionPane.showMessageDialog(null, "Seu texto tem " + resultado + " letras");
+
+    }
     
 }
