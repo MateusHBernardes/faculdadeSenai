4fd1b0ffc977c31f1236c3a7fc7a20ebfafbc020
mateushbernardes <mateushenriquebernardes@gmail.com>
2024-09-26 21:27:10 -0300

    desafio01 for

---
+++ 4 - For/Desafio01.java
@@ -0,0 +1,32 @@
+import javax.swing.JOptionPane;
+
+public class Desafio01 {
+    public static void main(String[] args) {
+        
+        int numero[] = new int[2];
+
+        int pergunta = 0;
+
+        int quantidade = 0;
+
+        for(int i=0; i<2;i++){
+            numero[i] = Integer.parseInt(JOptionPane.showInputDialog("Informe o "+(i+1)+"° número: "));
+        }
+
+        for(int i=0;i<1;i++){
+            int resultado = numero[0] * numero[1];
+            
+            pergunta = Integer.parseInt(JOptionPane.showInputDialog("Informe a multiplicação dos 2 valores: "));
+
+            while(pergunta != resultado) {
+                pergunta = Integer.parseInt(JOptionPane.showInputDialog( "Resultado errado"));
+
+                quantidade++;
+            }
+            
+            JOptionPane.showMessageDialog(null, "Parabéns, você acertou!");
+            
+            JOptionPane.showMessageDialog(null, "Quantidade de tentativas: "+quantidade);
+            }
+    }
+}
