090f65b529e5bde6951096009d8ac31524a4be33
mateushbernardes <mateushenriquebernardes@gmail.com>
2024-10-01 21:44:44 -0300

    validade matrix

--- 6 - Matrix/Matrix.java
+++ 6 - Matrix/Matrix.java
@@ -2,6 +2,7 @@
 import javax.swing.JOptionPane;
 
 public class Matrix {
+
     public static void main(String[] args) {
 
         int[][] matrix = new int[5][4];
@@ -10,32 +11,36 @@ public class Matrix {
 
         JOptionPane.showMessageDialog(null, "Soma de matriz");
 
-        for(int i=0; i<matrix.length; i++){
+        for (int i = 0; i < matrix.length; i++) {
 
-            for(int j=0; j<4; j++){
-                
-                matrix[i][j] = Integer.parseInt(JOptionPane.showInputDialog("Informe  o valor da matriz [" + i + "][" + j + "]")); 
+            for (int j = 0; j < 4; j++) {
 
-                total += matrix[i][j];
+                matrix[i][j] = Integer.parseInt(JOptionPane.showInputDialog("Informe  o valor da matriz [" + i + "][" + j + "]"));
 
             }
 
         }
 
-        for(int i=0; i< matrix.length; i++){
-            array =0;
+        for (int i = 0; i < matrix.length; i++) {
 
-            for(int j=0; j<4; j++){
+            array = 0;
+
+            for (int j = 0; j < 4; j++) {
                 array += matrix[i][j];
-                
-                somaArray[i] =  array;
+
+                somaArray[i] = array;
             }
         }
 
+        for (int i = 0; i < somaArray.length; i++) {
+
+            total += somaArray[i];
+
+        }
 
         JOptionPane.showMessageDialog(null, total);
-        for(int i = 0; i < somaArray.length; i++){
-            JOptionPane.showMessageDialog(null, "Soma da linha "+(i+1)+" é: " + somaArray[i]);
+        for (int i = 0; i < somaArray.length; i++) {
+            JOptionPane.showMessageDialog(null, "Soma da linha " + (i + 1) + " é: " + somaArray[i]);
 
         }
     }
--- 6 - Matrix/Matrix2.java
+++ 6 - Matrix/Matrix2.java
@@ -1,5 +1,49 @@
-
+import javax.swing.JOptionPane;
 
 public class Matrix2 {
-    
+    public static void main(String args[]) {
+
+        int[][] matrix = new int[3][2];
+        int[][] matrixTransposta = new int[2][3];
+
+        JOptionPane.showMessageDialog(null, "Transpor a matriz!");
+
+        int numero = 1;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                matrix[i][j] = numero++;
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                matrixTransposta[j][i] = matrix[i][j];
+            }
+        }
+
+
+        String matrixNormal = "";
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                matrixNormal += matrix[i][j] + " ";
+            }
+            matrixNormal += "\n";
+        }
+
+        JOptionPane.showMessageDialog(null, matrixNormal, "Matriz normal", JOptionPane.INFORMATION_MESSAGE);
+
+
+        String matrixT = "";
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrixT += matrixTransposta[i][j] + " ";
+            }
+            matrixT += "\n";
+        }
+
+        JOptionPane.showMessageDialog(null, matrixT, "Matriz transposta", JOptionPane.INFORMATION_MESSAGE);
+    }
 }
