7f0d8ed2c6622be709d31d3ce76f4dbde35a1baa
mateushbernardes <mateushenriquebernardes@gmail.com>
2024-10-02 21:46:10 -0300

    alteração

--- 7 - Methods/Methods02.java
+++ 7 - Methods/Methods02.java
@@ -1,18 +1,19 @@
+
 import javax.swing.JOptionPane;
 
 public class Methods02 {
 
-    public Double solicitar(){
+    public static Double solicitar() {
 
-    Double metros = Double.parseDouble(JOptionPane.showInputDialog(null, "Informe um valor em métros:"));
-    return metros;
+        Double metros = Double.parseDouble(JOptionPane.showInputDialog(null, "Informe um valor em métros:"));
+        return metros;
     }
 
-    public void escolherOpcao(Double metros){
+    public static void escolherOpcao(Double metros) {
 
-        int opcao =  Integer.parseInt(JOptionPane.showInputDialog(null, "1 - centímetros\n2 - polegadas\n3 - pés\n4 - quilômetros"));
+        int opcao = Integer.parseInt(JOptionPane.showInputDialog(null, "1 - centímetros\n2 - polegadas\n3 - pés\n4 - quilômetros"));
 
-        switch(opcao){
+        switch (opcao) {
             case 1:
                 metros = metros * 100;
                 JOptionPane.showMessageDialog(null, "O valor em centímetros é: " + metros);
@@ -24,12 +25,12 @@ public class Methods02 {
                 break;
 
             case 3:
-                metros  = metros * 3.28084;
+                metros = metros * 3.28084;
                 JOptionPane.showMessageDialog(null, "O valor em pés é: " + metros);
                 break;
 
             case 4:
-                metros  = metros * 0.001;
+                metros = metros * 0.001;
                 JOptionPane.showMessageDialog(null, "O valor em quilômetros é: " + metros);
                 break;
 
@@ -41,10 +42,9 @@ public class Methods02 {
 
     public static void main(String[] args) {
 
-    Methods02 metodo = new Methods02();
+//    Methods02 metodo = new Methods02();
+        Double metros = solicitar();
+        escolherOpcao(metros);
 
-    Double metros = metodo.solicitar();
-    metodo.escolherOpcao(metros);
-
-    }    
+    }
 }
